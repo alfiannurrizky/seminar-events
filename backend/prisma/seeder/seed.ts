518a676d13a612255b3a15ca336a9c546f9dd581
alfiannrzky <alfiannrzky0@gmail.com>
2023-07-10 10:36:54 +0700

    create user seeder

--- backend/prisma/seeder/seed.ts
+++ backend/prisma/seeder/seed.ts
@@ -4,27 +4,35 @@ import bcrypt from 'bcrypt'
 const prisma = new PrismaClient()
 
 async function main() {
-    const student = await prisma.student.createMany({
-        data: [
-            {
-                majorId: '7c17d8bd-2963-4167-9c39-94997596e973',
-                username: 'alfian',
-                email: 'alfi@email.com',
-                password: await bcrypt.hash('password', 10)
-            },
-            {
-                majorId: '68a6bc86-8aaa-4da1-9c7f-37e5528de3c2',
-                username: 'indah',
-                email: 'indah@email.com',
-                password: await bcrypt.hash('password', 10)
-            },
-            {
-                majorId: 'c5c0766f-aa3d-433d-9f02-2022916329ce',
-                username: 'ayu',
-                email: 'ayu@email.com',
-                password: await bcrypt.hash('password', 10)
-            }
-        ]
+    // const student = await prisma.student.createMany({
+    //     data: [
+    //         {
+    //             majorId: '7c17d8bd-2963-4167-9c39-94997596e973',
+    //             username: 'alfian',
+    //             email: 'alfi@email.com',
+    //             password: await bcrypt.hash('password', 10)
+    //         },
+    //         {
+    //             majorId: '68a6bc86-8aaa-4da1-9c7f-37e5528de3c2',
+    //             username: 'indah',
+    //             email: 'indah@email.com',
+    //             password: await bcrypt.hash('password', 10)
+    //         },
+    //         {
+    //             majorId: 'c5c0766f-aa3d-433d-9f02-2022916329ce',
+    //             username: 'ayu',
+    //             email: 'ayu@email.com',
+    //             password: await bcrypt.hash('password', 10)
+    //         }
+    //     ]
+    // })
+
+    const user = await prisma.user.create({
+        data: {
+            username: 'admin',
+            email: 'admin@email.com',
+            password: await bcrypt.hash('password', 10)
+        }
     })
 }
 
